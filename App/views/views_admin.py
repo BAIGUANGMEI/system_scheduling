e43a411647743e0649955f9f736a45c9f3af93ed
BAIGUANGMEI <2868653801@qq.com>
2023-10-11 00:08:27 +0800

    init_v1.1

--- App/views/views_admin.py
+++ App/views/views_admin.py
@@ -15,11 +15,45 @@ def login():
         try:
             if user == None:
                 error = '用户名或密码错误'
-                return redirect("/schedulesystem/admin/login/",flag=flag)
+                return redirect("/schedulesystem/admin/login/")
             else:
                 session['user_id'] = user.id
-                return redirect("/schedulesystem/index",flag=flag)
+                return redirect("/schedulesystem/index")
         except Exception as e:
             return 'add user fail' + str(e)
     elif request.method =='GET':
         return render_template('/admin/admin_login.html',flag=flag)
+
+@admin.route('/schedulesystem/admin/register/',methods=['GET','POST'])
+def register():
+    flag = False
+    if session.get('user_id'):
+        flag = True
+    if request.method =='GET':
+        return render_template('/admin/admin_register.html',flag=flag)
+    else:
+        username=request.form.get('username')
+        password=request.form.get('password')
+        confirm=request.form.get('confirm')
+        un = User.query.filter_by(username=username).first()
+        user = User(username=username,password=password)
+        try:
+            if password == confirm and un == None:
+                db.session.add(user)
+                db.session.commit()
+                return redirect('/schedulesystem/index/')
+            elif username != None:
+                error = '用户名已存在'
+                return render_template('/admin/admin_register.html',flag=flag)
+            else:
+                error = '密码不一致'
+                return render_template('/admin/admin_register.html',flag=flag)
+        except Exception as e:
+            db.session.rollback()
+            db.session.flush()
+            return 'add user fail' + str(e)
+
+@admin.route('/schedulesystem/admin/logout/')
+def logout():
+    session.clear()
+    return redirect('/schedulesystem/index/')
--- App/views/views_front.py
+++ App/views/views_front.py
@@ -1,18 +1,34 @@
-from flask import Blueprint,render_template,request,redirect,session,url_for
+from flask import Blueprint, render_template, request, redirect, session, url_for
 from ..models.models_front import *
+from ..models.models_admin import *
 
 front = Blueprint('front', __name__)
 
+
+# 网站首页接口
 @front.route('/schedulesystem/index/', methods=['GET', 'POST'])
 def index():
     flag = False
     if session.get('user_id'):
         flag = True
-    return render_template('front/front_index.html',flag=flag)
+    user_id = session.get('user_id')
+    user = User.query.filter_by(id=user_id).first()
+    return render_template('front/front_index.html', flag=flag, user=user)
 
+
+# 门店信息界面接口
 @front.route('/schedulesystem/introduction/', methods=['GET', 'POST'])
 def introduction():
     flag = False
     if session.get('user_id'):
         flag = True
-    return render_template('front/front_introduction.html',flag=flag)
+    user_id = session.get('user_id')
+    user = User.query.filter_by(id=user_id).first()
+    return render_template('front/front_introduction.html', flag=flag, user=user)
+
+# 门店信息界面客流量数据接口
+@front.route('/schedulesystem/introduction/graph/', methods=['GET'])
+def graph():
+    return {'code': 200, 'data_morning': [5, 20, 36, 10, 10, 20,10],
+            'data_afternoon':[7, 21, 16, 10, 10, 20,20],
+            'data_evening':[46, 30, 36, 45, 25, 70,33]}
